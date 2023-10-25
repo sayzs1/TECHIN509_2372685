0edc7018871d1afe8a3be2c960c32932d4f926e7
mkw18 <mkw22@mails.tsinghua.edu.cn>
2023-10-24 21:37:05 -0700

    mkw changes

--- flood_fill_function.py
+++ flood_fill_function.py
@@ -6,10 +6,11 @@ board = [
     "......#........#......",
     "......#........#......",
     "......#........#####..",
-    "....###............#..",
-    "....#............###..",
-    "....##############....",
+    "#######............#..",
+    ".................###..",
+    "##################....",
 ]
+
 def flood_fill(input_board: List[str], old: str, new: str, x: int, y: int) -> List[str]:
     """Returns board with old values replaced with new values
     through flood filling starting from the coordinates x, y
@@ -23,6 +24,16 @@ def flood_fill(input_board: List[str], old: str, new: str, x: int, y: int) -> Li
         List[str]: Modified board
     """
     # Implement your code here.
+    # Assert inputs are of right format
+    assert type(input_board[0]) == str
+    width = len(input_board[0])
+    for s in input_board:
+        assert len(s) == width
+        assert type(s) == str
+    assert type(old) == str
+    assert type(new) == str
+    assert type(x) == int
+    assert type(y) == int
 
     if x < 0 or x >= len(input_board) or y < 0 or y >= len(input_board[0]):
         return input_board
